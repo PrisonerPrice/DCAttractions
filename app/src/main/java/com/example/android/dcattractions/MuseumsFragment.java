bdfba844bc382ddaac0c84a40e435957682582f5
Hangbo <zhzhanghangbo@hotmail.com>
2019-12-20 02:28:58 -0500

    Implement new adapter in one fragment

--- app/src/main/java/com/example/android/dcattractions/MuseumsFragment.java
+++ app/src/main/java/com/example/android/dcattractions/MuseumsFragment.java
@@ -1,10 +1,15 @@
 package com.example.android.dcattractions;
 
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,7 +22,11 @@ import java.util.ArrayList;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MuseumsFragment extends Fragment {
+public class MuseumsFragment extends Fragment implements PlaceRecyclerAdapter.MyClickListener {
+
+    private static PlaceRecyclerAdapter placeRecyclerAdapter;
+    private static RecyclerView recyclerView;
+    private Context context;
 
 
     public MuseumsFragment() {
@@ -29,7 +38,10 @@ public class MuseumsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View rootView = inflater.inflate(R.layout.place_list, container, false);
+        context = getActivity();
+
+        //View rootView = inflater.inflate(R.layout.place_list, container, false);
+        View rootView = inflater.inflate(R.layout.recyclerview, container, false);
 
         final ArrayList<Place> places = new ArrayList<Place>();
 
@@ -54,24 +66,38 @@ public class MuseumsFragment extends Fragment {
                 R.drawable.museums_smithsonian_american_art_museum,38.897393,-77.022946));
 
         // Set list color
-        PlaceAdapter adapter = new PlaceAdapter(getActivity(), places, R.color.colorPrimary);
+        //PlaceAdapter adapter = new PlaceAdapter(getActivity(), places, R.color.colorPrimary);
 
-        ListView listView = (ListView) rootView.findViewById(R.id.list);
-        listView.setAdapter(adapter);
+        placeRecyclerAdapter = new PlaceRecyclerAdapter(places, this);
+        recyclerView = rootView.findViewById(R.id.recycler_view);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                Place place = places.get(position);
-                String geoInformation = "geo:"+ place.getGeoX() + "," + place.getGeoY() + "?q=" + place.getPlaceName();
-                Uri mUri = Uri.parse(geoInformation);
-                Intent mIntent = new Intent(Intent.ACTION_VIEW,mUri);
-                startActivity(mIntent);
-            }
-        });
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(context, 1);
 
-        // Inflate the layout for this fragment
-        return rootView;
+        recyclerView.setLayoutManager(gridLayoutManager);
+
+        recyclerView.setHasFixedSize(true);
+        
+        recyclerView.setAdapter(placeRecyclerAdapter);
+
+//        ListView listView = (ListView) rootView.findViewById(R.id.list);
+//
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+//                Place place = places.get(position);
+//                String geoInformation = "geo:"+ place.getGeoX() + "," + place.getGeoY() + "?q=" + place.getPlaceName();
+//                Uri mUri = Uri.parse(geoInformation);
+//                Intent mIntent = new Intent(Intent.ACTION_VIEW,mUri);
+//                startActivity(mIntent);
+//            }
+//        });
+//
+//        // Inflate the layout for this fragment
+          return rootView;
     }
 
+    @Override
+    public void onItemClick(int position) {
+        Log.d("XXXX","You are click something");
+    }
 }
